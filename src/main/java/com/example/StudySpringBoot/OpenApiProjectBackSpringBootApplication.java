16187475753d08c67d0b49dbfa1b65878e3b42c7
Chang-Young Park <parkcy0630@gmail.com>
2024-09-10 09:45:15 +0900

    first-commit

---
+++ src/main/java/com/example/StudySpringBoot/OpenApiProjectBackSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiProjectBackSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiProjectBackSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudySpringBoot/OpenApiProjectBackSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiProjectBackSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
